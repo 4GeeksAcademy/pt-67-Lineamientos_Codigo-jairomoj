576ce66b08a62c1691f87c66052bbe2b1336d677
jairomoj <58726090+jairomoj@users.noreply.github.com>
2024-05-26 15:30:33 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,7 +6,12 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  document.getElementById("excuse").innerHTML = excuseGenerator();
+
+  console.log("Hello Rigo from the console!");
+};
+
+function excuseGenerator() {
   let who = ["The dog", "My grandma", "The mailman", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "my phone", "the car"];
@@ -18,14 +23,13 @@ window.onload = function() {
     "while I was praying"
   ];
 
-  let randomExcuse = "";
-
-  randomExcuse += who[Math.floor(Math.random() * who.length)];
-  randomExcuse += " " + action[Math.floor(Math.random() * action.length)];
-  randomExcuse += " " + what[Math.floor(Math.random() * what.length)];
-  randomExcuse += " " + when[Math.floor(Math.random() * when.length)];
-
-  document.getElementById("excuse").innerHTML = randomExcuse;
-
-  console.log("Hello Rigo from the console!");
-};
+  return (
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)]
+  );
+}
